512fdef06862f912d9e86362552408f620b1f42a
Nikita Nikitenko <nickitenko.job@gmail.com>
2018-11-24 22:01:34 +0200

    makeup ceilings type + how we work section

--- app/js/common.js
+++ app/js/common.js
@@ -1,13 +1,22 @@
 $(function() {
 
-  $('#lightgallery').lightGallery();
+    $('#lightgallery').lightGallery();
 
-  $('.what-get-slider').slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  arrows: true
-});
+    $('.what-get-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true
+    });
+
+    if ($(window).width() > 768 && $('.ceilings-type-wrapper-item').length > 11) {
+        $('.ceilings-type-wrapper-item:gt(-5)').hide();
+    }
+
+    $('.show-more').on('click', function() {
+        $('.ceilings-type-wrapper-item:gt(-5)').toggle();
+        $(this).hide();
+    });
 
 
 
