c082f3d563d5d32e3b744da8a5b7dcddd4aa8431
Nikita Nikitenko <nickitenko.job@gmail.com>
2018-11-25 12:00:39 +0200

    makeup manufacture + team sections

--- app/js/scripts.min.js
+++ app/js/scripts.min.js
@@ -9,6 +9,18 @@ $(function() {
         arrows: true
     });
 
+    $('#manufacture-lightgallery').lightGallery();
+
+    $('#certificates').lightGallery();
+
+    $('.manufacture-wrapper-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: true
+    });
+
     if ($(window).width() > 768 && $('.ceilings-type-wrapper-item').length > 11) {
         $('.ceilings-type-wrapper-item:gt(-5)').hide();
     }
@@ -16,6 +28,12 @@ $(function() {
     $('.show-more').on('click', function() {
         $('.ceilings-type-wrapper-item:gt(-5)').toggle();
         $(this).hide();
+    });
+
+    $('.show-certificates').on('click', function() {
+      $('.manufacture-certificates-hide').toggle();
+
+      $(this).text() === 'показать сертификаты' ? $(this).text('скрыть сертификаты') : $(this).text('показать сертификаты');
     });
 
 
